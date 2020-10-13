f1c22f7ca9254982292fe6c8494914446174a04f
Chakavak <raoofpick@gmail.com>
2020-10-13 16:00:32 +0330

    show status service in dashboard

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-import os,subprocess
+import os,subprocess,sys,subprocess
 # Create your views here.
+import json
 
+import ast
 
 
 def Index(request):
@@ -11,9 +13,32 @@ def Index(request):
 
 
 def MonitoringService(request):
+    input_file = open ('config/json/service_names.json')
+    json_array = json.load(input_file)
+
+    elements={}
+    json_builder={"alldata": []}
+
     
+    for service_name in json_array["service"]:
+        
+        
+        
+        State=str(subprocess.Popen(["systemctl", "show", "-p", "SubState", "--value", service_name["name"]],stdout=subprocess.PIPE).communicate())
+        
     
-    return render (request,"Dashboard_Templates/ServiceMonitoring.html",{'alldata':[{'name':'Nginx','status':'Up','uptime':'75'},{'name':'Apache','status':'Down','uptime':'50'}]})
+        if "running" in State or "exited" in State:
+            elements[service_name["name"]]="Up"
+        else:
+            elements[service_name["name"]]="Down"
+        
+        
+
+
+    json_builder["alldata"].append((elements))
+
+    
+    return render (request,"Dashboard_Templates/ServiceMonitoring.html",json_builder)
 
 
 
