8013e09cc65a24fcff853388f219ca625ad23df0
Raouf Baghejari <r.baghjeri@chakavak.info>
2020-10-24 15:18:21 +0330

    edit models

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -2,29 +2,66 @@ from django.db import models
 
 # Create your models here.
 
+Type = ( 
+    ("ssw", "SSW"), 
+    ("sbc","SBC"),
+   
+) 
 
-
-class Service(models.Model):
-    service_id=models.AutoField(primary_key=True,unique=True,null=False)
+class Server(models.Model):
+    server_id=models.AutoField(primary_key=True,unique=True,null=False)
     name=models.CharField(max_length=50)
-    Type=models.CharField(max_length=50)
     ip=models.GenericIPAddressField()
-
+    Type=models.CharField(max_length=20,choices=Type,default='ssw')
     def __str__(self):
-        return str(self.service_id)
+        return str(self.server_id)
+
+ 
+    
+class SBCConfig(models.Model):
+    config_id=models.AutoField(primary_key=True,unique=True,null=False)
+    config=models.FileField(upload_to='Dashboard/static/config_files/SBC',null=True)
+    def __str__(self):
+        return str("config id " + str(self.config_id))
+    
+     
     
     
+    
+class SSWConfig(models.Model):
+    config_id=models.AutoField(primary_key=True,unique=True,null=False)
+    config=models.FileField(upload_to='Dashboard/static/config_files/SSW',null=True)
+    def __str__(self):
+        return str("config id " + str(self.config_id))
     
 
-class SoftSwitch(models.Model):
-    soft_id=models.AutoField(primary_key=True,unique=True,null=False)
-    service_id=models.OneToOneField(Service,unique=True,null=False,on_delete=models.CASCADE)
+
+
+class SSW(models.Model):
+    ssw_id = models.AutoField(primary_key=True,unique=True,null=False)
+    server_id=models.OneToOneField(Server,on_delete=models.CASCADE)
+    config=models.OneToOneField(SSWConfig,on_delete=models.CASCADE)
+    
     def __str__(self):
-        return str(self.service_id.service_id)
+        return str( "server id :"+ (str(self.server_id.server_id)))
+
 
 
 class SBC(models.Model):
-    sbc_id=models.AutoField(primary_key=True,unique=True,null=False)
-    service_id=models.OneToOneField(Service,unique=True,null=False,on_delete=models.CASCADE)
+    sbc_id = models.AutoField(primary_key=True,unique=True,null=False)
+    server_id=models.OneToOneField(Server,on_delete=models.CASCADE)
+    config=models.OneToOneField(SBCConfig,on_delete=models.CASCADE)
+    
     def __str__(self):
-        return str(self.service_id.service_id)
+        return str( "server id :"+ (str(self.server_id.server_id)))
+
+
+
+
+# class SBC(models.Model):
+#     sbc_id=models.AutoField(primary_key=True,unique=True,null=False)
+#     service_id=models.OneToOneField(Service,unique=True,null=False,on_delete=models.CASCADE)
+#     def __str__(self):
+#         return str(self.service_id.service_id)
+
+
--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -5,12 +5,12 @@ from Dashboard import views
 
 urlpatterns=[
     path("",views.MainDashboardPage),
-    path("<slug:slug>",views.MonitoringService),
-    #  path('start-service/', views.StartService),
-    #  path('stop-service/',views.StopService),
-    path('add-service/',views.AddService),
-    path('edit-service/',views.EditService),
-    path('deleting-service/',views.DeleteService),
+    path("<slug:slug>",views.MonitoringServer),
+    # #  path('start-service/', views.StartService),
+    # #  path('stop-service/',views.StopService),
+    path('add-service/',views.AddServer),
+    path('edit-service/',views.EditServer),
+    path('deleting-service/',views.DeleteServer),
 
 ]
 
